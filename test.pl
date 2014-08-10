57da6bb6cd6a5bfd2a488d95d7187c6580fb308a
tomoche <tomoche@outlook.com>
2014-08-10 17:57:04 +0900

    Add new sub_rootin.
    Add sub_rootin(Space Padding)

--- test.pl
+++ test.pl
@@ -1,7 +1,8 @@
 #!usr/bin/perl
 
-
-
+#######################
+# 0 padding
+#######################
 sub pad_(){
 my $char_len;
 my $pad_val;
@@ -21,3 +22,29 @@ $formated = $pad.$[0];
 
 return($formated);
 }
+
+
+#######################
+# Space padding
+#######################
+sub pad_(){
+sub pad_s(){
+my $char_len;
+my $pad_val;
+my $pad;
+my $formated;
+my $i;
+
+$char_len = length($_[0]);
+$pad_val = $_[1] - $char_len;
+
+$pad ="";
+for($i=0;$i<$pad_val,$i++){
+$pad = $pad." ";
+}
+
+$formated = $pad.$[0];
+
+return($formated);
+}
+
